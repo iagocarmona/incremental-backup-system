e04d30702c97b980ac98de072dd1b0de92bbb868
Gustavo Martins <gtavomartins893@gmail.com>
2023-09-18 19:55:18 -0300

    add status message colors for client

---
+++ cmd/client/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/fatih/color"
+)
+
+func main() {
+
+	// For status messages
+	color.Blue("Hello World!")  // starting some action/command/function
+	color.Green("Hello World!") // finished with success
+	color.Red("Hello World!")   // finished with error
+}
