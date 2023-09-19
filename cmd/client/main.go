5737a0d417881e94251ef372da2c8bb5c1e6a5e4
Gustavo Martins <gtavomartins893@gmail.com>
2023-09-18 22:07:28 -0300

    add initial workflow logic

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -10,4 +10,10 @@ func main() {
 	color.Blue("Hello World!")  // starting some action/command/function
 	color.Green("Hello World!") // finished with success
 	color.Red("Hello World!")   // finished with error
+
+	// 1 - Encontrar o diretório (especifiado no comando) na máquina local
+	// 2 - Percorrer esse diretório:
+	//			Existe algo dentro? (root != nil)
+	// 				Percorrer dirEntries[] verificando:
+	// 					se (é um file) e (ainda não existe no servidor) -> copiar para o servidor ( insert_file() )
 }
