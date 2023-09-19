0a7c60d964a9958d47bb4690449d6c8f47cb6273
Gustavo Martins <gtavomartins893@gmail.com>
2023-09-19 10:57:07 -0300

    add more workflow logic

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -15,5 +15,12 @@ func main() {
 	// 2 - Percorrer esse diretório:
 	//			Existe algo dentro? (root != nil)
 	// 				Percorrer dirEntries[] verificando:
-	// 					se (é um file) e (ainda não existe no servidor) -> copiar para o servidor ( insert_file() )
+
+	// 					se (é um file)
+	//						se (ainda não existe no servidor) -> copiar para o servidor ( insert_file() )
+	//                  	se (existe no servidor) e (foi modificado) -> atualizar no servidor ( update_file() )
+	//		                Edge Case: como verificar se um arquivo que foi apagado da máquina local existe no servidor? acho que essa verificação para realizar o delete_file() não ocorre aqui
+
+	//                  se (é um dir)
+	// 						se (ainda não existe no servidor) -> criar no servidor ( insert_node() )
 }
